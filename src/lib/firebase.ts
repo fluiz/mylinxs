5761737b23993bf75c128f9d870ce1fc770c9a95
Fabio Luiz <luiz.fabio@gmail.com>
2024-05-01 21:58:32 -0300

    Initial commit

---
+++ src/lib/firebase.ts
@@ -0,0 +1,52 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { writable } from "svelte/store";
+import { type User, onAuthStateChanged } from "firebase/auth"
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDUQSvx8HvG585IxbSoSiPbS0KWU8UqfWQ",
+    authDomain: "mylinxs-bc6bc.firebaseapp.com",
+    projectId: "mylinxs-bc6bc",
+    storageBucket: "mylinxs-bc6bc.appspot.com",
+    messagingSenderId: "734662996738",
+    appId: "1:734662996738:web:88ec1bd334145eebf5881b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
+export const auth = getAuth();
+export const storage = getStorage();
+
+/**
+ * @returns a store with the current firebase user
+ */
+function userStore() {
+    let unsubscribe: () => void;
+
+    if (!auth || !globalThis.window) {
+        console.warn('Auth is not initialized or not in browser');
+        const { subscribe } = writable<User | null>(null);
+        return {
+            subscribe
+        }
+    }
+
+    const { subscribe } = writable(auth?.currentUser ?? null, (set) => {
+        unsubscribe = onAuthStateChanged(auth, (user) => {
+            set(user);
+        });
+
+        return () => unsubscribe();
+    });
+
+    return {
+        subscribe
+    };
+}
+
+export const user = userStore();
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import "../app.css";
+    import { goto } from '$app/navigation';
+
+
+
+    // function moveOn() {
+    //     goto('login');
+    // }
+</script>
+
+<div class="min-h-screen flex flex-col">
+<!--    <button class="btn" on:click={moveOn}>Move on</button>-->
+    <slot />
+</div>
